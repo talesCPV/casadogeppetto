23278c6341c5133e391ead3787f8cbc79e8e081f
talesCPV <tales@flexibus.com.br>
2022-06-29 16:51:24 -0300

    by_script

---
+++ files/query_db.php
@@ -0,0 +1,57 @@
+<?php
+
+    $query_db = array(
+         "0" => 'SELECT * FROM tb_usuario WHERE y00="x00" AND y01="x01";',
+         "1" => "INSERT INTO tb_usuario VALUES ('a','b','c');",
+              
+    );
+
+
+    if (IsSet($_POST["cod"]) && IsSet($_POST["params"]) && IsSet($_POST["token"]) ){
+
+        $cod = $_POST["cod"];        
+        $params = json_decode($_POST["params"],true); 
+        $token = $_POST["token"];
+
+        include "connect.php";        
+/*        
+        if($cod == 0){ // login
+            $mytoken = crip($params["user"].date("h:i:s"));
+            $query = "UPDATE tb_user SET token = '{$mytoken}' WHERE user = '{$params['user']}' AND pass = '{$params['pass']}';";
+            mysqli_query($conexao, $query);
+        }
+*/
+        $query = $query_db[$_POST["cod"]];
+        $i = 0;
+        foreach($params as $key => $val ){
+            $y = 'y'.str_pad(strval($i), 2, "0", STR_PAD_LEFT);
+            $x = 'x'.str_pad(strval($i), 2, "0", STR_PAD_LEFT);
+        
+            $query = str_replace($y, $key,$query); // fields
+            $query = str_replace($x, $val,$query); // values
+            $i++;
+        }
+
+//        echo $query;
+
+		$result = mysqli_query($conexao, $query);
+		$qtd_lin = $result->num_rows;
+
+		if($qtd_lin > 0){
+			$rows = array();
+			while($r = mysqli_fetch_assoc($result)) {
+			    $rows[] = $r;
+			}
+//            array_push($rows,"teste");
+			print json_encode($rows);
+
+
+
+		}
+
+	    $conexao->close();        
+
+    }
+
+
+?>
