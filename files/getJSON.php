5a1c8dc91f537b3ae2f726cabab1ac7cf655a04c
talesCPV <tales@flexibus.com.br>
2022-06-29 23:09:04 -0300

    by_script -> qua 29 jun 2022 23:09:04 -03

---
+++ files/getJSON.php
@@ -0,0 +1,41 @@
+<?php   
+
+  $out = [];
+
+
+	if (IsSet($_POST["path"])){
+	  $path = $_POST["path"];
+	  $access = $_POST["access"];
+	  $category = $_POST["category"];
+
+      if (file_exists($path)) {
+          $fp = fopen($path, "r");
+          $resp = "";
+          while (!feof ($fp)) {
+              $resp = $resp . fgets($fp,4096);
+          }
+          fclose($fp);
+
+          $json = json_decode($resp);
+
+          for($i = 0; $i< count($json->frame); $i++){
+            if($json->frame[$i]->category <= $category){
+                $obj = new stdClass();
+                $obj->content = $json->frame[$i]->content;
+                $obj->filename = $json->frame[$i]->filename;
+                $obj->background = $json->frame[$i]->background;
+                $obj->justify = $json->frame[$i]->justify;
+                $obj->font = $json->frame[$i]->font;
+                $obj->text = $json->frame[$i]->text;
+                array_push($out, json_encode($obj));
+            }            
+
+          }
+
+        }
+    }
+
+//    var_dump($out);
+	print json_encode($out);
+
+?>
