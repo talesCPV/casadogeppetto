00587eff3a087b8dfe28a89191a5cf46a583337c
talesCPV <tales@flexibus.com.br>
2022-07-12 11:51:37 -0300

    by_script

--- files/query_db.php
+++ files/query_db.php
@@ -1,16 +1,21 @@
 <?php
 
     $query_db = array(
-         "0" => 'SELECT * FROM tb_usuario WHERE y00="x00" AND y01="x01";',
-         "1" => 'INSERT INTO tb_usuario (y00, y01, y02, y03, y04, y05) VALUES ("x00", "x01", "x02", "x03", "x04", "x05");',
-         "2" => 'SELECT * FROM tb_frames WHERE y00="x00" AND y01<="x01" ORDER BY id DESC;',
-         "3" => 'INSERT INTO tb_frames (y00, y01, y02, y03, y04, y05) VALUES ("x00", "x01", "x02", "x03", "x04", "x05");',
-         "4" => 'UPDATE tb_frames SET y00="x00", y01="x01", y02="x02", y03="x03", y04="x04", y05="x05" WHERE y06="x06";',
-         "5" => 'DELETE FROM tb_frames  WHERE y00="x00";',
-         "6" => 'SELECT * FROM tb_usuario as A  WHERE(SELECT B.access from tb_usuario as B WHERE hash = "x00") = 10;',
-         "7" => 'UPDATE tb_usuario SET y00="x00", y01="x01", y02="x02", y03="x03", y04="x04", y05="x05" WHERE y06="x06";',
-         "8" => 'DELETE FROM tb_usuario WHERE y00="x00";',
-         "9" => 'INSERT INTO tb_brinquedo (y00, y01, y02, y03, y04) VALUES ("x00", "x01", "x02", "x03", "x04");',
+         "0"  => 'SELECT * FROM tb_usuario WHERE y00="x00" AND y01="x01";',
+         "1"  => 'INSERT INTO tb_usuario (y00, y01, y02, y03, y04, y05) VALUES ("x00", "x01", "x02", "x03", "x04", "x05");',
+         "2"  => 'SELECT * FROM tb_frames WHERE y00="x00" AND y01<="x01" ORDER BY id DESC;',
+         "3"  => 'INSERT INTO tb_frames (y00, y01, y02, y03, y04, y05) VALUES ("x00", "x01", "x02", "x03", "x04", "x05");',
+         "4"  => 'UPDATE tb_frames SET y00="x00", y01="x01", y02="x02", y03="x03", y04="x04", y05="x05" WHERE y06="x06";',
+         "5"  => 'DELETE FROM tb_frames  WHERE y00="x00";',
+         "6"  => 'SELECT * FROM tb_usuario as A  WHERE(SELECT B.access from tb_usuario as B WHERE hash = "x00") = 10;',
+         "7"  => 'UPDATE tb_usuario SET y00="x00", y01="x01", y02="x02", y03="x03", y04="x04", y05="x05" WHERE y06="x06";',
+         "8"  => 'DELETE FROM tb_usuario WHERE y00="x00";',
+         "9"  => 'INSERT INTO tb_brinquedo (y00, y01, y02, y03, y04) VALUES ("x00", "x01", "x02", "x03", "x04");',
+         "10" => 'SELECT * FROM tb_brinquedo WHERE y00 LIKE "%x00%"',
+         "11" => 'UPDATE tb_brinquedo SET y00="x00", y01="x01", y02="x02", y03="x03", y04="x04" WHERE y05="x05";',
+         "12" => 'DELETE FROM tb_brinquedo WHERE y00="x00";',
+         "13" => 'INSERT INTO tb_festa (y00, y01, y02) VALUES ("x00", "x01", "x02");',
+         "14" => 'SELECT F.* FROM tb_festa as F INNER JOIN tb_usuario as U WHERE F.y00="x00" AND F.id_user = U.id;',
               
     );
 
