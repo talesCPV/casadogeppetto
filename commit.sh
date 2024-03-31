ac50e0073db7ac6423837419b3eb2a7d77bdcb9b
talesCPV <tales@flexibus.com.br>
2024-03-31 00:05:40 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -17,10 +17,10 @@ then
 
     git commit -m "by_script"
 
-#    git branch -M main
+    git branch -M master
 #    git remote add origin git@github.com:talesCPV/casadogeppetto.git
     git remote set-url origin git@github.com:talesCPV/casadogeppetto.git
 
-    git push -u -f origin main
+    git push -u -f origin master
 
 fi
